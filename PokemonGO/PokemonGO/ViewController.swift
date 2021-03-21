06e2310af5fee1afa0eb65d105e3ec1e85fca105
Gilberto Silva <gilberto.silva@sigga.com>
2021-03-20 22:59:27 -0300

    Configurando a solicitação de permissão a localização e tratando fluxos caso o usuário não dê a permissão.

--- PokemonGO/PokemonGO/ViewController.swift
+++ PokemonGO/PokemonGO/ViewController.swift
@@ -8,15 +8,87 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    private var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initLocationManager()
     }
-
+    
+    private func initLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let locationLast = locations.last {
+            applyZoomRegion(latitude: locationLast.coordinate.latitude, longitude: locationLast.coordinate.longitude)
+        }
+    }
+    
+    private func applyZoomRegion(latitude: CLLocationDegrees,
+                                 longitude: CLLocationDegrees) {
+        
+        let deltaLatitude: CLLocationDegrees = 0.01
+        let deltaLongitude: CLLocationDegrees = 0.01
+        
+        let location = CLLocationCoordinate2DMake(latitude, longitude)
+        let areaVisualization = MKCoordinateSpan(latitudeDelta: deltaLatitude, longitudeDelta: deltaLongitude)
+        
+        let region =  MKCoordinateRegion(center: location, span: areaVisualization)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+        let status =  manager.authorizationStatus
+        switch status {
+        case .notDetermined:
+            print("Status: notDetermined")
+        case .restricted:
+            print("Status: restricted")
+        case .denied:
+            print("Status: denied")
+            showAlertRequestPermissionLocation()
+        case .authorizedAlways:
+            print("Status: authorizedAlways")
+        case .authorizedWhenInUse:
+            print("Status: authorizedWhenInUse")
+            locationManager.startUpdatingLocation()
+        @unknown default:
+            print("Status: nil")
+        }
+    }
+    
+    private func showAlertRequestPermissionLocation() {
+        let alertController = UIAlertController(title: "Permissão de localização",
+                                                message: "Necessário permissão para à sua localização!",
+                                                preferredStyle: .actionSheet)
+        addAlertActions(alertController)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func addAlertActions(_ alertController: UIAlertController) {
+        let configurationAction = UIAlertAction(title: "Abrir configurações",
+                                                style: .default) { (action) in
+            
+            if let bundleId = Bundle.main.bundleIdentifier,
+               let url = URL(string: "\(UIApplication.openSettingsURLString)&path=LOCATION/\(bundleId)"),
+               UIApplication.shared.canOpenURL(url)
+            {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+        
+        alertController.addAction(configurationAction)
+        alertController.addAction(cancelAction)
+    }
 
 }
 
