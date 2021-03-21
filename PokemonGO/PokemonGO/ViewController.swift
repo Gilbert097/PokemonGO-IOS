7a1133af7be40e09f50c94cc6f9a4e8f9e1016cb
Gilberto Silva <gilberto.silva@sigga.com>
2021-03-20 22:28:58 -0300

    Criando projeto.

---
+++ PokemonGO/PokemonGO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonGO
+//
+//  Created by Gilberto Silva on 20/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
